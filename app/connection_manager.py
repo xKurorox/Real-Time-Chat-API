945ba88066bffe5068aee449cfe3238910c86bff
Marvin Gomez <marvingomez@Marvins-MacBook-Pro.local>
2026-04-13 21:52:32 -0700

    Added connection manager with multi-client broadcasting

---
+++ app/connection_manager.py
@@ -0,0 +1,14 @@
+class ConnectionManager():
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message):
+        for websocket in self.active_connections:
+            await websocket.send_text(message)
--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from app.connection_manager import ConnectionManager
 
 app = FastAPI()
+manager = ConnectionManager()
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Message was sent: {data}")
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast("Someone has left the chat")
