e120ebff779b75e9b57162e070a8af47eb930ecf
Marvin Gomez <marvingomez@Marvins-MacBook-Pro.local>
2026-04-14 21:30:00 -0700

    Added database schema for users, rooms, and messages

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from app.connection_manager import ConnectionManager
+from app.database import engine, Base, SessionLocal
+from app.models import User, Room, UserRoom, Message
 
 app = FastAPI()
 manager = ConnectionManager()
+Base.metadata.create_all(bind = engine)
 
 
 @app.websocket("/ws")
---
+++ app/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = False)
+    email = Column(String, nullable = True, unique = True)
+    messages = relationship("Message", back_populates = "user")
+    rooms = relationship("Room", back_populates = "users", secondary = "user_rooms")
+
+class Room(Base):
+    __tablename__ = "rooms"
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = False, unique = True)
+    users = relationship("User", back_populates = "rooms", secondary = "user_rooms")
+    messages = relationship("Message", back_populates = "room")
+
+class Message(Base):
+    __tablename__ = "messages"
+    id = Column(Integer, primary_key = True)
+    text = Column(String, nullable = False)
+    created_at = Column(DateTime, default = lambda: datetime.now(timezone.utc))
+    user_id = Column(Integer, ForeignKey("users.id"), index = True)
+    room_id = Column(Integer, ForeignKey("rooms.id"), index = True)
+    user = relationship("User", back_populates = "messages")
+    room = relationship("Room", back_populates = "messages")
+
+class UserRoom(Base):
+    __tablename__ = "user_rooms"
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey("users.id"), index = True)
+    room_id = Column(Integer, ForeignKey("rooms.id"), index = True)
+    join_at = Column(DateTime, default = lambda: datetime.now(timezone.utc))
